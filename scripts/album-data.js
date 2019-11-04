76cc82eea0d21655164e0807608e6040edd09a80
Lloyed <lloyedcho@gmail.com>
2019-11-04 01:16:13 -0700

    checkpoint-1 done

--- scripts/album-data.js
+++ scripts/album-data.js
@@ -11,3 +11,7 @@ const album = {
     { title: 'Magenta', duration: '374.22', soundFileUrl: 'assets/music/magenta.mp3' }
   ]
 };
+{
+  $('#album-title').text(album.title);
+  $('img#album-cover-art').attr('src', album.albumArtUrl);
+}
